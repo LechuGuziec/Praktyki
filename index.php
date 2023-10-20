1b8dd8425ac5858249c88b25e96d2283a2e6cf71
Guziec <guziec@gmail.com>
2023-10-20 08:59:49 +0200

    Szkielet folderów

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 require 'vendor/autoload.php';?>
-<!doctype html>
-<html lang="pl">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Praktyki</title>
-</head>
-<body>
 
-</body>
-</html>
+<?php include 'szablony/naglowek.php'?>
+
+<?php include 'szablony/stopka.php'?>
