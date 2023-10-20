685a5001424758b88d2e025f0c1267b50ebd455e
Guziec <guziec@gmail.com>
2023-10-20 08:53:44 +0200

    Start projektu

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Praktyki</title>
+</head>
+<body>
+
+</body>
+</html>
